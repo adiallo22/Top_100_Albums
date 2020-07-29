0f8912865a4687d0d05b7bbbe34b8779d8df4bde
abdul diallo <rahimi.diallo96@gmail.com>
2020-07-29 15:10:25 -0400

    remove storyboard entrypoint. Create Feed ViewController and make it root

---
+++ Top_100_Albums/Controllers/Feed.swift
@@ -0,0 +1,18 @@
+//
+//  Feed.swift
+//  Top_100_Albums
+//
+//  Created by Abdul Diallo on 7/29/20.
+//  Copyright © 2020 Abdul Diallo. All rights reserved.
+//
+
+import UIKit
+
+class Feed : UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .orange
+    }
+    
+}
