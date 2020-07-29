2d03c85ec527e98318a8192b17b08692a42324ad
abdul diallo <rahimi.diallo96@gmail.com>
2020-07-29 16:05:50 -0400

    create tableview. Add delegate and datasource protocols. set constraints of the tableview. Test with an array

--- Top_100_Albums/Controllers/Feed.swift
+++ Top_100_Albums/Controllers/Feed.swift
@@ -8,11 +8,57 @@
 
 import UIKit
 
+private let cellIdentifier = "albumCell"
+
 class Feed : UIViewController {
+    
+    private let tableView = UITableView()
+    
+    private let test = ["x", "x", "x"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .orange
+        view.backgroundColor = .white
+        configTableView()
     }
+    
+}
+
+//MARK: - helpers
+
+extension Feed {
+    
+    func configTableView() {
+        view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        tableView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        tableView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(AlbumCell.self, forCellReuseIdentifier: cellIdentifier)
+    }
+    
+}
+
+//MARK: - delegate and data source
+
+extension Feed : UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("selected.")
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return test.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier) as! AlbumCell
+        cell.textLabel?.text = test[indexPath.row]
+        return cell
+    }
+    
     
 }
---
+++ Top_100_Albums/View/AlbumCell.swift
@@ -0,0 +1,21 @@
+//
+//  AlbumCell.swift
+//  Top_100_Albums
+//
+//  Created by Abdul Diallo on 7/29/20.
+//  Copyright © 2020 Abdul Diallo. All rights reserved.
+//
+
+import UIKit
+
+class AlbumCell : UITableViewCell {
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
