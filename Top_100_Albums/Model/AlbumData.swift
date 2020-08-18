79de720755745c9fb98bd8915a2dc746f1977efc
abdul diallo <rahimi.diallo96@gmail.com>
2020-08-18 18:03:30 -0400

    add custom struct conforming to codable

---
+++ Top_100_Albums/Model/AlbumData.swift
@@ -0,0 +1,28 @@
+//
+//  AlbumData.swift
+//  Top_100_Albums
+//
+//  Created by Abdul Diallo on 8/17/20.
+//  Copyright © 2020 Abdul Diallo. All rights reserved.
+//
+
+import Foundation
+
+//let genre : String
+
+struct AlbumJSON : Codable {
+    
+    struct FeedJSON : Codable {
+        struct Result : Codable {
+            var artistName : String
+            struct Genre : Codable {
+                var name : String
+            }
+            var genres : Genre
+            var url : String
+        }
+        var results : [Result]
+    }
+    var feed : FeedJSON
+    
+}
